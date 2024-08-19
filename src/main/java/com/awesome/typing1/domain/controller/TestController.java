d065095c7b53d995a580c5d3bb6372966221492f
leesj <jinycap7@gmail.com>
2024-08-19 18:51:12 +0900

    [FEAT] 스웨거 및 Base Entity 설정

---
+++ src/main/java/com/awesome/typing1/domain/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.awesome.typing1.domain.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
