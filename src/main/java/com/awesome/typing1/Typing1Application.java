7ed2dd417cb8353a1d39117cf68a641d7406d886
leesj <jinycap7@gmail.com>
2024-08-19 16:01:00 +0900

    기본 스프링 환경 세팅

---
+++ src/main/java/com/awesome/typing1/Typing1Application.java
@@ -0,0 +1,13 @@
+package com.awesome.typing1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Typing1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Typing1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/awesome/typing1/Typing1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome.typing1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Typing1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
